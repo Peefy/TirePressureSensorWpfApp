1e5c191f89d34d9fe1c6541dde274d788bf0ff44
Peefy <xpf6677@163.com>
2019-08-02 16:21:28 +0800

    Initial commit

---
+++ TirePressureSensorWpfApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ArkLight.Mvvm;
+
+namespace TirePressureSensorWpfApp.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string _windowTitle = "轮胎压力传感器上位机";
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set => SetProperty(ref _windowTitle, value);
+        }
+    }
+}
